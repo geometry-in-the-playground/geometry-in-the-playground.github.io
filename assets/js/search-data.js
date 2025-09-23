b6fd30c950d89a5c093737466684cd1792151130
theodumont <dumont.theo@protonmail.com>
2025-09-23 14:13:22 +0000

    Deploying to gh-pages from @ geometry-in-the-playground/geometry-in-the-playground.github.io@487d03a36a255211f5957d4752df8ce5b5e1a181 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-postal-horn-the-0th-edition-of-the-workshop-took-place-in-annecy",
+          title: ':postal_horn:  The 0th edition of the workshop took place in Annecy!',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
